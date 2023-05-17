7d3a33132ece514ddfb9824f2cfe49ad5ac25c41
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-05-17 19:12:01 -0300

    criando um arquivo para as rotas de memories da aplicação

---
+++ src/routes/memories.ts
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+import { FastifyInstance } from 'fastify'
+// import { prisma } from '../lib/prisma'
+// import z from 'zod'
+
+export async function memoriesRoutes(app: FastifyInstance) {
+  
+}
