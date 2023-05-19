26ce09ce35b0cf45635765db9771f69131ef9dc1
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-05-19 14:44:54 -0300

    registrando o jwt no server

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import fastify from 'fastify'
 import { memoriesRoutes } from './routes/memories'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 import 'dotenv/config'
 import { authRoutes } from './routes/auth'
 
@@ -8,6 +9,9 @@ const app = fastify()
 
 app.register(cors, {
   origin: true,
+})
+app.register(jwt, {
+  secret: 'spacetime',
 })
 app.register(authRoutes)
 app.register(memoriesRoutes)
