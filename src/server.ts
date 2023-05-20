3232f083cd1d058d9f86e0f238c5738140f6b241
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-05-19 23:23:36 -0300

    registrando a rota de upload e configurando o fastify/static no server

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,17 @@ import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
 import 'dotenv/config'
 import { authRoutes } from './routes/auth'
+import multipart from '@fastify/multipart'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
 
 const app = fastify()
 
+app.register(multipart)
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 app.register(cors, {
   origin: true,
 })
@@ -15,6 +23,7 @@ app.register(jwt, {
 })
 app.register(authRoutes)
 app.register(memoriesRoutes)
+app.register(uploadRoutes)
 
 app
   .listen({
